cfe8259ddd074a698e095e5831ecc2752adf3678
Simeon Králik <simeon.kralik@gmail.com>
2023-04-10 20:17:15 +0200

    Select nearest available expire time of the status and small improvements

--- selenium/actions.py
+++ selenium/actions.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timedelta
 from selenium.webdriver.remote.webdriver import WebDriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.action_chains import ActionChains
@@ -45,9 +46,11 @@ class Actions:
         emoji_picker_input.send_keys(emoji_name)
 
         def is_emoji_first(driver: WebDriver):
-            first_emoji = driver.find_element(By.CSS_SELECTOR, f'.p-emoji_picker__list_scroller img:first-of-type')
-            return first_emoji.get_attribute("data-stringify-emoji") == emoji_name
-
+            try:
+                first_emoji = driver.find_element(By.CSS_SELECTOR, f'.p-emoji_picker__list_scroller img:first-of-type')
+                return first_emoji.get_attribute("data-stringify-emoji") == emoji_name
+            except Exception:
+                return
         self._wait.until(is_emoji_first)
         emoji_picker_input.send_keys(Keys.RETURN)
 
@@ -65,10 +68,31 @@ class Actions:
     def click_save_button(self):
         self._wait_and_click(By.CSS_SELECTOR, 'button[data-qa="custom_status_input_go"]')
 
+    def click_status_duration_dropdown(self):
+        self._wait_and_click(By.CSS_SELECTOR, '#custom_status_duration_default_button')
+
+    def click_choose_date_and_time_option(self):
+        self._wait.until(EC.visibility_of_element_located([By.CSS_SELECTOR, '#custom_status_duration_default_listbox']))
+        options = self._driver.find_element(By.CSS_SELECTOR, '#custom_status_duration_default_listbox')
+        option = options.find_element(By.XPATH, '//div[@role="option"]/*[contains(text(), "Choose date and time")]')
+        self._action.move_to_element(option).click().perform()
+
+    def update_time_picker(self, song_duration):
+        """DOESN'T WORK. Slack only lets you select the times from the dropdown.
+        Keeping it in case Slack gets an update.
+        Updates the time picker with the time of the song end
+        (not accurate, since we don't have the current time of the song - could be done in the future).
+        """
+        self._wait_and_click(By.CSS_SELECTOR, 'div[data-qa="date_time_picker_time_picker"]')
+        time_picker = self._driver.find_element(By.CSS_SELECTOR, 'div[data-qa="date_time_picker_time_picker"]')
+        end_time = datetime.now() + timedelta(milliseconds=song_duration)
+        end_time = end_time.strftime('%I:%M %p')
+        self._action.send_keys_to_element(time_picker, end_time, Keys.ENTER).perform()
+
     def escape(self, count=1):
         for _ in range(count):
-            self._driver.implicitly_wait(1)
             self._action.send_keys(Keys.ESCAPE).perform()
+            self._driver.implicitly_wait(1)
 
     def _wait_and_click(self, *locator):
         self._wait.until(EC.visibility_of_element_located(locator))
--- selenium/main.py
+++ selenium/main.py
@@ -8,40 +8,48 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.remote.webdriver import WebDriver
 
-REFRESH_INTERVAL = os.getenv('REFRESH_INTERVAL')
+REFRESH_INTERVAL = int(os.getenv('REFRESH_INTERVAL'))
 SPOTIFY_CLIENT_ID = os.getenv('SPOTIFY_CLIENT_ID')
 SPOTIFY_CLIENT_SECRET = os.getenv('SPOTIFY_CLIENT_SECRET')
+DEBUG = bool(os.getenv('DEBUG'))
 
 EMOJI = os.getenv('EMOJI')
-Track = namedtuple('Track', 'name,artist')
+Track = namedtuple('Track', 'name,artist,duration_ms')
 
 def update_status(driver: WebDriver, track: Track):
-    actions = Actions(driver)
-    actions.escape(3)
-    actions.click_avatar()
+    try:
+        actions = Actions(driver)
+        actions.escape(3)
+        actions.click_avatar()
 
-    new_status = track.name + " by " + track.artist if track else None
-    if actions.get_current_status_text() == new_status:
-        return
+        new_status = track.name + " by " + track.artist if track else None
+        if actions.get_current_status_text() == new_status:
+            return
 
-    status_emoji_name = actions.get_current_status_emoji()
-    if status_emoji_name is None and track is None:
-        return
-    elif status_emoji_name is not None and status_emoji_name != EMOJI:
-        print("Slack status not updated, because it would override a possibly more important status.")
-        return
+        status_emoji_name = actions.get_current_status_emoji()
+        if status_emoji_name is None and track is None:
+            return
+        elif status_emoji_name is not None and status_emoji_name != EMOJI:
+            print("Slack status not updated, because it would override a possibly more important status.")
+            return
 
-    status_emoji_name = actions.click_update_status_button()
+        status_emoji_name = actions.click_update_status_button()
 
-    if not track:
-        actions.click_clear_all_button()
-        actions.click_save_button()
-        print("Status cleared.")
-        return
+        if not track:
+            actions.click_clear_all_button()
+            actions.click_save_button()
+            print("Status cleared.")
+            return
 
-    actions.click_emoji_picker_button()
-    actions.select_emoji(EMOJI)
-    actions.update_status(new_status)
+        actions.click_emoji_picker_button()
+        actions.select_emoji(EMOJI)
+        actions.click_status_duration_dropdown()
+        actions.click_choose_date_and_time_option()
+        actions.update_status(new_status)
+    except Exception as e:
+        if not DEBUG:
+            raise e
+        print(str(e))
 
 
 def get_current_track(sp: spotipy.Spotify):
@@ -50,7 +58,8 @@ def get_current_track(sp: spotipy.Spotify):
     if current_track is not None and 'item' in current_track:
         track_name = current_track['item']['name']
         artist_name = current_track['item']['artists'][0]['name']
-        track = Track(track_name, artist_name)
+        duration = current_track['item']['duration_ms']
+        track = Track(track_name, artist_name, duration)
         return track
     else:
         print("No song is currently playing on Spotify.")
@@ -66,7 +75,7 @@ def is_slack_workspace_open(driver: WebDriver):
         driver.switch_to.window(window_handle)
         if 'https://app.slack.com/' in driver.current_url:
             return True
-        elif 'okta.com' in driver.current_url:
+        elif 'okta.com' in driver.current_url:  # TODO: Handle different auth providers
             return False  # Stop switching windows when we are being authenticated
 
     return False
@@ -86,6 +95,6 @@ with webdriver.Chrome() as driver:
     update_status(driver, track)
 
     while True:
-        time.sleep(10)
+        time.sleep(REFRESH_INTERVAL)
         track = get_current_track(sp)
         update_status(driver, track)
