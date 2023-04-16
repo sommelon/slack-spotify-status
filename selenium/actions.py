bcfc61d563b0de2acbed4afec9a66c190c4e9322
Simeon Králik <simeon.kralik@gmail.com>
2023-04-16 22:02:58 +0200

    Support Mac OS and try to make emoji picker more consistently

--- selenium/actions.py
+++ selenium/actions.py
@@ -6,6 +6,9 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
+import platform
+os_base = platform.system()
+
 
 class Actions:
     def __init__(self, driver: WebDriver, wait: WebDriverWait):
@@ -44,17 +47,29 @@ class Actions:
         self._wait.until(EC.visibility_of_element_located([By.CSS_SELECTOR, '.p-emoji_picker__input']))
         emoji_picker_input = self._driver.find_element(By.CSS_SELECTOR, '.p-emoji_picker__input')
         emoji_picker_input.send_keys(emoji_name)
+        self._driver.implicitly_wait(2)
 
         def is_emoji_first(driver: WebDriver):
             first_emoji = driver.find_element(By.CSS_SELECTOR, f'.p-emoji_picker__list_scroller img:first-of-type')
             return first_emoji.get_attribute("data-stringify-emoji") == emoji_name
+        self._wait.until(is_emoji_first)
+
+        if os_base == 'Darwin':
+            emoji_picker_input.send_keys(Keys.COMMAND, 'A')
+        else:
+            emoji_picker_input.send_keys(Keys.CONTROL, 'A')
+        emoji_picker_input.send_keys(emoji_name)  # repeat emoji name, because sometimes the list gets reset
         self._wait.until(is_emoji_first)
         emoji_picker_input.send_keys(Keys.RETURN)
 
     def update_status(self, status):
         self._wait.until(EC.visibility_of_element_located([By.CSS_SELECTOR, 'div[aria-label="Status"]']))
         update_status_input = self._driver.find_element(By.CSS_SELECTOR, 'div[aria-label="Status"]')
-        update_status_input.send_keys(Keys.CONTROL, 'A')
+
+        if os_base == 'Darwin':
+            update_status_input.send_keys(Keys.COMMAND, 'A')
+        else:
+            update_status_input.send_keys(Keys.CONTROL, 'A')
         update_status_input.send_keys(status)
         update_status_input.send_keys(Keys.RETURN)
         print(f"Slack status updated (probably - too lazy to check ¯\_(ツ)_/¯). Status: {status}")
